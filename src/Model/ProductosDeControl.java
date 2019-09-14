11d53f60ca29f6c8b6523427384b6b86fb45fef5
SENBIAS <49045371+SENBIAS@users.noreply.github.com>
2019-09-14 18:32:07 -0500

    FASE 1

---
+++ src/Model/ProductosDeControl.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class ProductosDeControl {
+    //ATRIBUTOS
+    private String ica;
+    private String nombreProducto;
+    private int frecuenciaAplicacion;
+
+    //CONSTRUCTOR
+
+    public ProductosDeControl(String ica, String nombreProducto, int frecuenciaAplicacion) {
+        this.ica = ica;
+        this.nombreProducto = nombreProducto;
+        this.frecuenciaAplicacion = frecuenciaAplicacion;
+    }
+    
+    
+    //METODOS
+    public String getIca() {
+        return ica;
+    }
+
+    public void setIca(String ica) {
+        this.ica = ica;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getFrecuenciaAplicacion() {
+        return frecuenciaAplicacion;
+    }
+
+    public void setFrecuenciaAplicacion(int frecuenciaAplicacion) {
+        this.frecuenciaAplicacion = frecuenciaAplicacion;
+    }
+    
+    
+}
