6a86d98a505a55a08038f60685cc0dbf8fe97396
SENBIAS <49045371+SENBIAS@users.noreply.github.com>
2019-09-17 04:06:09 -0500

    TERMINADO

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,7 +6,6 @@
 package Main;
 
 import Model.*;
-import java.util.ArrayList;
 
 import java.util.Scanner;
 /**
@@ -28,108 +27,24 @@ public class Main
             option = tecladoInt.nextInt();
             switch (option) {
                 case 1:
-                    menuAgregar(inventario);
+                    Inventario.menuAgregar(inventario);
                     break;
                 case 2:
-                    buscar(inventario);
+                    Inventario.buscar(inventario);
                     break;
                 case 3:
-                    
+                    Inventario.actualizar(inventario);
+                    break;
+                case 4:
+                    Inventario.eliminar(inventario);
+                    break;
+                case 5:
+                    CRUD.mostrarProductos(inventario.getPlagas(), inventario.getFertilizantes());
+                    break;
+                case 6:
                     break;
             }
-        } while (true);
-    }
-    
-    private static void buscar(Inventario inventario){
-        String ica;
-        Scanner tecladoString = new Scanner(System.in);
-        System.out.println("Ingrese el ICA del producto a buscar ");
-        ica = tecladoString.nextLine();
-        ControlPlagas plaga = CRUD.buscarPorIcaPlaga(ica, inventario.getPlagas());
-        ControlFertilizante fertilizante = CRUD.buscarPorIcaFertilizante(ica, inventario.getFertilizantes());
-        if(plaga!=null){
-            System.out.println("EL PRODUCTO ES DE CONTROL DE PLAGAS.");
-            System.out.println("ICA: "+plaga.getIca());
-            System.out.println("Nombre: "+plaga.getNombreProducto());
-            System.out.println("Periodo de carencia: "+plaga.getPeriodoCarencia());
-            System.out.println("Frecuencia de aplicacion: "+plaga.getFrecuenciaAplicacion());
-        }else if(fertilizante != null){
-            System.out.println("EL PRODUCTO ES DE FERTILIZANTES.");
-            System.out.println("ICA: "+fertilizante.getIca());
-            System.out.println("Nombre: "+fertilizante.getNombreProducto());
-            System.out.println("Fecha ultima aplicacion: "+fertilizante.getFechaUltimaAplicacion());
-            System.out.println("Frecuencia de aplicacion: "+fertilizante.getFrecuenciaAplicacion());
-        }else{
-            System.out.println("EL PRODUCTO NO EXITE.");
-        }   
-    }
-    private static void menuAgregar(Inventario inventario){
-        int option;
-        String ica;
-        System.out.println("1.Agregar producto de plagas.");
-        System.out.println("2.Agregar producto fertilizante.");
-        Scanner tecladoInt = new Scanner(System.in);
-        option = tecladoInt.nextInt();
-        
-        switch (option) {
-            case 1:
-                agregarPlaga(inventario.getPlagas());
-                break;
-            case 2:
-                agregarFertilizante(inventario.getFertilizantes());
-                break;
-            default:
-                System.out.println("La opcion no existe.");
-                break;
-        }
-    }
-    
-    private static void agregarFertilizante(ArrayList<ControlFertilizante> listaFertiliantes){
-        String fechaUltimaAplicacion;
-        String ica;
-        String nombreProducto;
-        int frecuenciaAplicacion;
-        ControlFertilizante fertilizante;
-        
-        Scanner tecladoInt = new Scanner(System.in);
-        Scanner tecladoString = new Scanner(System.in);
-        
-        System.out.print("Ingrese el ICA: ");
-        ica = tecladoString.nextLine();
-        System.out.print("Ingrese el nombre del producto: ");
-        nombreProducto = tecladoString.nextLine();
-        System.out.print("Ingrese la ultima fecha de aplicacion: ");
-        fechaUltimaAplicacion = tecladoString.nextLine();
-        System.out.println("Ingrese la frecuencia de aplicacion: ");
-        frecuenciaAplicacion = tecladoInt.nextInt();
-        
-        fertilizante = new ControlFertilizante(fechaUltimaAplicacion, ica, nombreProducto, frecuenciaAplicacion);
-        
-        listaFertiliantes.add(fertilizante);
-    }
-    
-    private static void agregarPlaga(ArrayList<ControlPlagas> listaPlagas){
-        int periodoCarencia;
-        String ica;
-        String nombreProducto;
-        int frecuenciaAplicacion;
-        ControlPlagas plaga;
-        
-        Scanner tecladoInt = new Scanner(System.in);
-        Scanner tecladoString = new Scanner(System.in);
-        
-        System.out.print("Ingrese el ICA: ");
-        ica = tecladoString.nextLine();
-        System.out.print("Ingrese el nombre del producto: ");
-        nombreProducto = tecladoString.nextLine();
-        System.out.print("Ingrese el periodo de carencia: ");
-        periodoCarencia = tecladoInt.nextInt();
-        System.out.println("Ingrese la frecuencia de aplicacion: ");
-        frecuenciaAplicacion = tecladoInt.nextInt();
-        
-        plaga = new ControlPlagas(periodoCarencia, ica, nombreProducto, frecuenciaAplicacion);
-        
-        listaPlagas.add(plaga);
+        } while (option<6);
     }
     
     private static void menu() {
@@ -137,7 +52,8 @@ public class Main
         System.out.println("2.Buscar producto.");
         System.out.println("3.Actualizar producto.");
         System.out.println("4.Eliminar producto.");
-        System.out.println("5.Salir.");
+        System.out.println("5.Mostrar productos.");
+        System.out.println("6.Salir.");
     }
     
 }
